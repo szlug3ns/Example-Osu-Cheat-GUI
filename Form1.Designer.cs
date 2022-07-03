d37013644f65f7c9c0556231048d0d3d32aff299
Szlug3ns <jan7sitarski@outlook.com>
2022-07-04 00:25:51 +0200

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Example_Cheat_Menu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonPanel3 = new System.Windows.Forms.Button();
+            this.buttonPanel2 = new System.Windows.Forms.Button();
+            this.buttonPanel1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(804, 27);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Euclid Circular A Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "LABEL";
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Euclid Circular A Medium", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(694, -2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(44, 26);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Euclid Circular A Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(744, -2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonPanel3
+            // 
+            this.buttonPanel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.buttonPanel3.FlatAppearance.BorderSize = 0;
+            this.buttonPanel3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPanel3.Font = new System.Drawing.Font("Euclid Circular A Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonPanel3.ForeColor = System.Drawing.Color.White;
+            this.buttonPanel3.Location = new System.Drawing.Point(7, 209);
+            this.buttonPanel3.Name = "buttonPanel3";
+            this.buttonPanel3.Size = new System.Drawing.Size(118, 39);
+            this.buttonPanel3.TabIndex = 6;
+            this.buttonPanel3.Text = "FuncPanel3";
+            this.buttonPanel3.UseVisualStyleBackColor = false;
+            this.buttonPanel3.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // buttonPanel2
+            // 
+            this.buttonPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.buttonPanel2.FlatAppearance.BorderSize = 0;
+            this.buttonPanel2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPanel2.Font = new System.Drawing.Font("Euclid Circular A Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonPanel2.ForeColor = System.Drawing.Color.White;
+            this.buttonPanel2.Location = new System.Drawing.Point(7, 164);
+            this.buttonPanel2.Name = "buttonPanel2";
+            this.buttonPanel2.Size = new System.Drawing.Size(118, 39);
+            this.buttonPanel2.TabIndex = 7;
+            this.buttonPanel2.Text = "FuncPanel2";
+            this.buttonPanel2.UseVisualStyleBackColor = false;
+            this.buttonPanel2.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // buttonPanel1
+            // 
+            this.buttonPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.buttonPanel1.FlatAppearance.BorderSize = 0;
+            this.buttonPanel1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPanel1.Font = new System.Drawing.Font("Euclid Circular A Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonPanel1.ForeColor = System.Drawing.Color.White;
+            this.buttonPanel1.Location = new System.Drawing.Point(7, 119);
+            this.buttonPanel1.Name = "buttonPanel1";
+            this.buttonPanel1.Size = new System.Drawing.Size(118, 39);
+            this.buttonPanel1.TabIndex = 7;
+            this.buttonPanel1.Text = "FuncPanel1";
+            this.buttonPanel1.UseVisualStyleBackColor = false;
+            this.buttonPanel1.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Red;
+            this.panel2.Location = new System.Drawing.Point(132, 35);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(656, 403);
+            this.panel2.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.panel3.Location = new System.Drawing.Point(131, 35);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(657, 403);
+            this.panel3.TabIndex = 10;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Green;
+            this.panel4.Location = new System.Drawing.Point(131, 34);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(657, 404);
+            this.panel4.TabIndex = 11;
+            // 
+            // panel5
+            // 
+            this.panel5.BackgroundImage = global::Example_Cheat_Menu.Properties.Resources.KyLyrOAv_400x400;
+            this.panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel5.Location = new System.Drawing.Point(7, 35);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(118, 78);
+            this.panel5.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-3, 437);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "GUI Made By Szlug3ns";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.buttonPanel1);
+            this.Controls.Add(this.buttonPanel2);
+            this.Controls.Add(this.buttonPanel3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonPanel3;
+        private System.Windows.Forms.Button buttonPanel2;
+        private System.Windows.Forms.Button buttonPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example_Cheat_Menu
+{
+    public partial class Form1 : Form
+    {
+        private Point lastPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void logo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_KeyPress(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            panel4.BringToFront();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            panel3.BringToFront();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            panel2.BringToFront();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
